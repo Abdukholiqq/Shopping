7b5982c25ae4194af668e087db9f0b82a6eb944c
Abdukholiqq <sumusddollar@gmail.com>
2023-02-09 19:20:03 +0500

    single

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,39 +1,39 @@
 import React, { useState, useEffect } from "react";
 import Category from "./Halpers/Category";
 
-import { json, Link, useParams } from "react-router-dom";
+import { json, Link, useNavigate, useParams } from "react-router-dom";
 import Stars from "./Halpers/Stars";
 const Main = () => {
   const [card, setCard] = useState([]);
   const [rating, setRating] = useState({});
-  const [paramm , setParam]= useState()
-  const { productId } = useParams(); 
+  const [paramm, setParam] = useState();
+  const { productId } = useParams();
+  const navigate = useNavigate();
   useEffect(() => {
     //   https://api.escuelajs.co/api/v1/products/
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((data) =>
-      //  {
+        //  {
         //    const list = Object.keys(data.rating).reduce((acc, key) => {
-          //     acc[key] = data.rating[key];
-          //     return acc;
-          //   }, {});
-          //   setCard(data)
-          //    setRating(list)
-          // }
-          setCard(data)
-  );
+        //     acc[key] = data.rating[key];
+        //     return acc;
+        //   }, {});
+        //   setCard(data)
+        //    setRating(list)
+        // }
+        setCard(data)
+      );
   }, []);
-// console.log(paramm)
- 
+  // console.log(paramm
+
   return (
-    
     <div>
       <div className="d-flex justify-content-between container flex-wrap gap-3">
         {card.map((item) => {
           //  const url = item.title.replace(/\s+/g, "-");
           // console.log(item.id)
-        //  setParam(item.id)
+          //  setParam(item.id)
           return (
             <div
               key={item.id}
@@ -52,15 +52,14 @@ const Main = () => {
               </div>
 
               <h6 className="text-primary">{item.price}$</h6>
-              <Link to={"/products/" + item.id}>
-              
-                <button className="btn form-control bg-primary text-white">
-                  Read More
-                </button>
-              </Link>
+              <button
+                className="btn form-control bg-primary text-white"
+                onClick={() => navigate(`/products/${item.id}`)}>
+                Read More
+              </button>
             </div>
           );
-        }) }
+        })}
       </div>
     </div>
   );
--- src/Pages/Singlepage.jsx
+++ src/Pages/Singlepage.jsx
@@ -4,34 +4,31 @@ import React, { useEffect, useState } from "react";
 import { json, useParams } from "react-router-dom";
 import Stars from "../Components/Halpers/Stars";
 
-
 export default function Singlepage() {
-  const {productId} = useParams(); 
+  const { name } = useParams();
   const [json, setSingle] = useState([]);
   const [rating, setRating] = useState({});
-  // console.log(productId)
   useEffect(() => {
-    axios.get(`https://fakestoreapi.com/products/${productId}`).then((json) => {
-        // const list = Object.keys(json.rating).reduce((acc, key) => {
-        //   acc[key] = json.rating[key];
-        //   return acc;
-        // }, {});
+    const getdata = async () => {
+      const data = await axios.get(`https://fakestoreapi.com/products/${name}`);
 
-        setSingle(json);
-        // setRating(list)
-      });
-  }, [productId]); 
-  
-// console.log(json.id)
+      setSingle(data.data);
+    };
+
+    getdata();
+  }, [name]);
+
   return (
     <div className="container text-start">
       <div key={json.id} className="shadow mt-5  d-flex gap-5 p-3">
-        <img src={json.image} alt="image" height={450} />
-        <div className="w-50 px-5">
+        <img src={json.image} alt="image" height={300} />
+        <div className="w-50 px-5 d-flex flex-column justify-content-around">
           <h1>{json.title}</h1>
 
-          <h4><Stars/></h4>
-          {/* <h4>Comment {rating.count}</h4> */}
+          <h4>
+            <Stars />
+          </h4>
+          {/* <h4>Comment {json.rating.count}</h4> */}
           <del className="text-danger"> {Math.ceil(json.price + 6)}.9$ </del>
           <h4> {json.price} $</h4>
           <h6 className="text-secondary">
@@ -41,11 +38,15 @@ export default function Singlepage() {
             <h4>Status :</h4>
             <h6 className="mt-1 text-success">● Available for sale</h6>
             <div className="links"></div>
-            </div>
-            <div className="btns d-flex gap-3 ">
-              <button className="btn bg-primary text-white">Muddatli to'lov</button>
-              <button className="btn bg-primary text-white">Savatga Qo'shish</button>
-              <button className="btn bg-primary text-white"> Sotib olish</button>
+          </div>
+          <div className="btns d-flex gap-3 ">
+            <button className="btn bg-primary text-white">
+              Muddatli to'lov
+            </button>
+            <button className="btn bg-primary text-white">
+              Savatga Qo'shish
+            </button>
+            <button className="btn bg-primary text-white"> Sotib olish</button>
           </div>
         </div>
       </div>
